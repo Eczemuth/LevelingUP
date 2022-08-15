960a8e8264d8865b7e9d6a4545cf1b357499a113
Kittipan Onlay <61462393+Eczemuth@users.noreply.github.com>
2022-08-15 23:34:24 +0700

    Update sol2.c

--- week2/sol2.c
+++ week2/sol2.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 
 int main(){
-    int n,until = 1,prev_until;
-
+    int n,length = 1,prev_length;
+    // lenght is lenght of a line, prev lenght is use to save the old lenght
     scanf("%d",&n);
-    prev_until = until;
-    for(int i = 0;i < n*n;i++){
-        if(until > n) break;
-        if(until > 0){
+    // save the old lenght
+    prev_length = length;
+    // in put n gives (n*(n+1))/2 '*' in total
+    for(int i = 0;i < (n*(n+1))/2;i++){
+        // if length still exist printf '*' and decrease length by one
+        if(length > 0){
             printf("*");
-            until -= 1;
-        }            
-        else{
-            until = prev_until+1;
-            prev_until = until;
+            length -= 1;
+        }
+        // if length is zero caculate new length and new prev_lenth then print new line
+        if(length == 0){
+            length = prev_length+1;
+            prev_length = length;
             printf("\n");
         }
     }
