de59eb7d62bec40378d883a4f70e921615c42743
Kittipan Onlay <61462393+Eczemuth@users.noreply.github.com>
2022-08-15 23:01:28 +0700

    Update sol1.c

--- week2/sol1.c
+++ week2/sol1.c
@@ -4,11 +4,13 @@ int main(){
     int n;
 
     scanf("%d",&n);
-
+    // first for loop is how many lines will it print
     for(int i = 1;i <= n;i++){
+        // second for loop is how long will it print per line
         for(int j = 0;j < i;j++){
             printf("*");
         }
+        // when done printing this line print new line
         printf("\n");
     }
     
