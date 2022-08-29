7aa638ae93c431ad8f41d9e9dabaf17484bff4cf
Kittipan Onlay <61462393+Eczemuth@users.noreply.github.com>
2022-08-29 21:49:32 +0700

    Add files via upload

---
+++ week4/sol1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int prev = 0,n,sort = 0,count = 0;
+    do{
+        scanf("%d",&n);
+        if(n > prev) sort += 1;
+        prev = n;
+        count += 1;
+    }while(n != 999);
+    if(count == sort) printf("YES");
+    else printf("NO");
+    return 0;
+}
---
+++ week4/sol2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int prev = 0,n,sort = 1;
+    do{
+        scanf("%d",&n);
+        if(n < prev) sort = 0;
+        prev = n;
+    }while(n != 999);
+    if(sort) printf("YES");
+    else printf("NO");
+    return 0;
+}
