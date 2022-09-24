c40feb0dbad389fb35f934debdc8f4bdcb915de9
Kittipan Onlay <61462393+Eczemuth@users.noreply.github.com>
2022-09-25 01:36:57 +0700

    Update sol1.c

--- week4/sol1.c
+++ week4/sol1.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 
 int main(){
-    int prev = 0,n,sort = 0,count = 0;
+    int prev,n,sort = 0,count = 0,less = -1;
+    scanf("%d",&prev);
     do{
         scanf("%d",&n);
-        if(n >= prev || n <= prev) sort += 1;
+        if(n == 999) break;
+
+        if(less == -1 && n < prev) less = 1;
+        else if(less == -1 && n > prev) less = 0;
+
+        if(less && n < prev) sort += 1;
+        else if(!less && n > prev) sort += 1;
+
         prev = n;
         count += 1;
-    }while(n != 999);
+    }while(1);
     if(count == sort) printf("YES");
     else printf("NO");
     return 0;
